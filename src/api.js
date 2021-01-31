db461a0777b021462dab125b39b19bb8dcf531e1
Adam <adam@mradam.us>
2021-01-30 20:05:20 -0600

    create current data function for API add a key and API

---
+++ src/api.js
@@ -0,0 +1,39 @@
+//Getting the date
+//Current month
+const getCurrentMonth = () => {
+  const month = new Date().getMonth() + 1;
+  if (month < 10) {
+    return `0${month}`;
+  } else {
+    return month;
+  }
+};
+
+//Current Day
+const getCurrentDay = () => {
+  const day = new Date().getDate();
+  if (day < 10) {
+    return `0${day}`;
+  } else {
+    return day;
+  }
+};
+
+//Current day/month/year
+const currentYear = new Date().getFullYear();
+const currentMonth = getCurrentMonth();
+const currentDay = getCurrentDay();
+const currentDate = `${currentYear}-${currentMonth}-${currentDay}`;
+const lastYear = `${currentYear - 1}-${currentMonth}-${currentDay}`;
+const nextYear = `${currentYear + 1}-${currentMonth}-${currentDay}`;
+
+//Base URL
+const APIKey = process.env.REACT_APP_KEY;
+const base_url = `https://api.rawg.io/api/`;
+
+// Pouplar Games
+// https://api.rawg.io/api/games?key=YOUR_API_KEY&dates=2019-09-01,2019-09-30&platforms=18,1,7
+const popular_game = `games?key=${APIKey}&dates=${lastYear},${currentDate}&ordering=-rating&page_size=10`;
+
+const popularGameURL = () => `${base_url}${popular_game}`;
+console.log(popularGameURL());
